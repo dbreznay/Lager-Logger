ec5f1080da28be5f5562ed5cce60021a09d863f8
Shane Runnels <shanerunnels@gmail.com>
2020-10-15 19:02:07 -0500

    working with components

---
+++ client/src/components/Title/index.js
@@ -0,0 +1,33 @@
+import React from 'react';
+
+const style = {
+    title:{
+        "fontWeight":  "bolder",
+        "textAlign" :  "center",
+        "fontSize"  :  "4rem",
+        "color"      :  "#ffc821",
+        "textShadow":  "2px 2px 0px black",
+        "marginBottom" :  "5px"
+    },
+    description:{
+        "textAlign" :  "center",
+        "fontSize"  :  "1.2rem",
+        "color"      :  "white",
+        "textShadow":  "1px 1px 1px black",
+        "margin"    :  "0px"
+    }
+};
+
+const Title = (props) => {
+    return(
+        <div>
+            <h1 style={style.title}>
+                CIRCLE OF DEATH
+            </h1>
+            <p style={style.description}>
+                Play <b>Circle of Death</b> online with your friends. 
+            </p>
+        </div>        
+    );
+}
+export default Title;
