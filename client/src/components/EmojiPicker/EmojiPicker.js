332e9e50dc4fa7c8963d597436181a69fc89c5b9
Whitney Monk <whitneym92@gmail.com>
2020-10-18 18:28:08 -0500

    working on emoji page, changed title of app

--- client/src/components/EmojiPicker/EmojiPicker.js
+++ client/src/components/EmojiPicker/EmojiPicker.js
@@ -1,23 +1,32 @@
 import React, { useState } from 'react';
-import Picker from 'emoji-picker-react'; 
+import { render } from 'react-dom';
+import Picker, { SKIN_TONE_MEDIUM_DARK } from 'emoji-picker-react';
 
-const emojiObject = () => {
+
+const App = () => {
     const [chosenEmoji, setChosenEmoji] = useState(null);
 
     const onEmojiClick = (event, emojiObject) => {
         setChosenEmoji(emojiObject);
-    };
+    }
 
     return (
         <div>
-            {chosenEmoji ? (
-                <span>You chose: {chosenEmoji.emoji}</span>
-            ) : (
-                <span>No emoji Chosen</span>
-            )}
-            <Picker onEmojiClick={onEmojiClick} />
+            <Picker onEmojiClick={onEmojiClick} disableAutoFocus={true} skinTone={SKIN_TONE_MEDIUM_DARK} groupNames={{smileys_people:"PEOPLE"}}/>
+            { chosenEmoji && <EmojiData chosenEmoji={chosenEmoji}/>}
         </div>
-    )
-}
+    );
+};
 
-export default emojiObject;
+const EmojiData = ({chosenEmoji}) => (
+  <div>
+    <strong>Unified:</strong> {chosenEmoji.unified}<br/>
+    <strong>Names:</strong> {chosenEmoji.names.join(', ')}<br/>
+    <strong>Symbol:</strong> {chosenEmoji.emoji}<br/>
+    <strong>ActiveSkinTone:</strong> {chosenEmoji.activeSkinTone}
+  </div>
+);
+
+render(<App />, document.getElementById('root'));
+
+export default EmojiPicker;
