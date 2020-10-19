a73e5616b053bbb22ac6f9b83ffe23851b3c4ea5
dbreznay <60904436+dbreznay@users.noreply.github.com>
2020-10-19 18:58:54 -0500

    switched routing and adding styles

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,11 @@
-import React, { Component } from 'react'
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
-import Game from './components/Game';
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Game from './components/Game/index';
 import './App.css';
-import Header from './components/header/Header'
-import Auth from './pages/Auth'
-import Profile from './pages/Profile';
+import Header from './components/header/Header';
+import Home from './components/Home/Home';
+import Auth from './pages/Auth';
+
 
  
 class App extends Component {
@@ -13,12 +14,12 @@ class App extends Component {
       <Router>
         <>
             <Header/>
-            <Game/>
+          
             <Switch>
-
+              <Route exact path='/home' component={Home} />
               <Route exact path='/login' render={(props) => <Auth {...props} action="login" />} />
               <Route exact path="/signup" render={(props) => <Auth {...props} action="signup" />} />
-              <Route exact path="/profile" component={Profile} />
+              <Route exact path="/game" component={Game} />
 
             </Switch>
         </>
